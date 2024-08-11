1f9735b622fcd9c629590b80fdfb3e86b64c7f4f
NAVIN CHAUDHARY <154901815+Navin-Chaudhary@users.noreply.github.com>
2024-08-11 22:50:45 +0530

    git code push

--- src/components/not-found/Not_found.jsx
+++ src/components/not-found/Not_found.jsx
@@ -9,10 +9,10 @@ function Not_found() {
     navigate('/')
   }
   return (
-    <div className="  bg-[#040816] text-[#9e9ea4] min-h-screen p-3 pt-0 overflow-x-hidden">
+    <div className="  bg-[#09080A] text-[#9e9ea4] min-h-screen p-3 pt-0 overflow-x-hidden">
       <div className="max-w-3xl mx-auto">
         <Navbar/>
-        <div className='min-h-[calc(100vh_-_165px)] flex justify-center flex-col items-center'>
+        <div className='min-h-[calc(100vh_-_100px)] flex justify-center flex-col items-center'>
         <h1 className="text-5xl bg-white  text-black rounded-lg cursor-not-allowed"><BiError/></h1>
       <h1 className='text-2xl mt-3 text-white font-bold'>404 | Page Not Found</h1>
       <p className='text-sm mt-2'>This page has been moved or was never created.</p>
