5c9f92b1b93defbbdd4cc26e6d6ab8b06009f5ee
NAVIN CHAUDHARY <154901815+Navin-Chaudhary@users.noreply.github.com>
2024-08-11 14:14:59 +0530

    font changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link, NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-[#040816]">
-      <div className="max-w-3xl mx-auto px-3">
+    <nav className="fixed top-0 left-0 right-0 z-50 ">
+      <div className="max-w-3xl mx-auto px-3 lg:px-0 backdrop-blur-3xl">
         <div className="flex items-center justify-between h-16">
           <div>
             <Link
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,14 +9,14 @@ function Projects() {
         Projects
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4">
-        <div className=" border-[1px] border-[#939396] p-4 rounded-lg project_card">
+        <div className=" border-[1px] border-[#939396] p-4 rounded-lg project_card ">
           <Link
             to="https://gituser.navin.bio"
-            className="font-bold border-b-[1px] tracking-wider flex items-center gap-1  transition-all duration-300 hover:border-gray-500   w-max text-lg text-white cursor-pointer"
+            className="font-bold border-b-[1px] tracking-wider flex items-center gap-1  transition-all duration-300 hover:border-gray-500   w-max text-lg text-white cursor-pointer hover:translate-y-[-2px] "
           >
             Gituser <span className="hover:translate-y-[-2px] hover:text-blue-500 transition-all duration-300"><FaExternalLinkAlt /></span>
           </Link>
-          <p className="text-[#c2c1c1] capitalize mt-3">
+          <p className="text-[#c2c1c1] capitalize mt-3 hover:translate-y-[-2px] transition-all duration-300">
             Gituser is a website that allows you to enter a username and get all
             relevant information about a GitHub user or organization, including
             their followers, following, and repositories.
@@ -25,11 +25,11 @@ function Projects() {
         <div className="border-[1px] border-[#939396]  p-4 rounded-lg project_card2">
           <Link
             to="https://weatherapp.navin.bio"
-            className="font-bold border-b-[1px] tracking-wider flex items-center gap-1  transition-all duration-300 hover:border-gray-500  w-max text-lg text-white cursor-pointer"
+            className="font-bold border-b-[1px] tracking-wider flex items-center gap-1  transition-all duration-300 hover:border-gray-500  w-max text-lg text-white cursor-pointer hover:translate-y-[-2px]"
           >
             Weather App <span className="hover:translate-y-[-2px] hover:text-blue-500 transition-all duration-300"><FaExternalLinkAlt /></span>
           </Link>
-          <p className="text-[#c2c1c1] capitalize mt-3 ">
+          <p className="text-[#c2c1c1] capitalize mt-3 hover:translate-y-[-2px] transition-all duration-300">
             weather app is a website where you will find multiple tools on a
             single page, which includes week's forecast, Temperature, Weather
             mode,uv, Wind, Pressure, Humidity, Sunset/sunrise time and much
@@ -39,11 +39,11 @@ function Projects() {
         <div className="border-[1px] border-[#939396]  p-4 rounded-lg project_card">
           <Link
             to="https://coffeweb.navin.bio"
-            className="font-bold border-b-[1px] tracking-wider flex items-center gap-1  transition-all duration-300 hover:border-gray-500  w-max text-lg text-white cursor-pointer"
+            className="font-bold border-b-[1px] tracking-wider flex items-center gap-1  transition-all duration-300 hover:border-gray-500  w-max text-lg text-white cursor-pointer hover:translate-y-[-2px]"
           >
             CoffeWeb <span className="hover:translate-y-[-2px] hover:text-blue-500 transition-all duration-300"><FaExternalLinkAlt /></span>
           </Link>
-          <p className="text-[#c2c1c1] capitalize mt-3">
+          <p className="text-[#c2c1c1] capitalize mt-3 hover:translate-y-[-2px] transition-all duration-300">
             News is a React app where you will find news highlights. This data
             is provided by the NYT API and you can sort news by category and you
             can also search for articles.This app showcases my skills in React,Tailwind and JavaScript.
@@ -53,11 +53,11 @@ function Projects() {
         <div className="border-[1px] border-[#939396]  p-4 rounded-lg project_card2">
           <Link
             to="https://to-do.navin.bio"
-            className="font-bold border-b-[1px] tracking-wider flex items-center gap-1  transition-all duration-300 hover:border-gray-500  w-max text-lg text-white cursor-pointer"
+            className="font-bold border-b-[1px] tracking-wider flex items-center gap-1  transition-all duration-300 hover:border-gray-500  w-max text-lg text-white cursor-pointer hover:translate-y-[-2px]"
           >
             To-do List <span className="hover:translate-y-[-2px] hover:text-blue-500 transition-all duration-300"><FaExternalLinkAlt /></span>
           </Link>
-          <p className="text-[#c2c1c1] capitalize mt-3">
+          <p className="text-[#c2c1c1] capitalize mt-3 hover:translate-y-[-2px] transition-all duration-300">
             These projects highlight my ability to create dynamic and responsive
             user interfaces, manage state efficiently and implement create,
             read, delete operations.This app showcases my skills in HTML, CSS
@@ -67,11 +67,11 @@ function Projects() {
         <div className="border-[1px] border-[#939396]  p-4 rounded-lg project_card">
           <Link
             to="https://news.navin.bio"
-            className="font-bold border-b-[1px] tracking-wider flex items-center gap-1  transition-all duration-300 hover:border-gray-500  w-max text-lg text-white cursor-pointer"
+            className="font-bold border-b-[1px] tracking-wider flex items-center gap-1  transition-all duration-300 hover:border-gray-500  w-max text-lg text-white cursor-pointer hover:translate-y-[-2px]"
           >
             News <span className="hover:translate-y-[-2px] hover:text-blue-500 transition-all duration-300"><FaExternalLinkAlt /></span>
           </Link>
-          <p className="text-[#c2c1c1] capitalize mt-3">
+          <p className="text-[#c2c1c1] capitalize mt-3 hover:translate-y-[-2px] transition-all duration-300">
             News is a React app where you will find news highlights. This data
             is provided by the NYT API and you can sort news by category and you
             can also search for articles.
