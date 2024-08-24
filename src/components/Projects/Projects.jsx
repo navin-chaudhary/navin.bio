7d88f9fdaa30aaf606afb53bf76b687b03b45f10
Navin-Chaudhary <heynavin.im@gmail.com>
2024-08-24 19:43:55 +0530

    git code push

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,6 +8,8 @@ import "swiper/css";
 import "swiper/css/effect-cards";
 import "swiper/css/pagination";
 import git1 from "/Images/git1.png";
+import weather1 from "/Images/weather1.png";
+import weather2 from "/Images/weather2.png";
 function Projects() {
   const projects = [
     {
@@ -17,56 +19,35 @@ function Projects() {
       url: "https://gituser.navin.bio",
       description:
         "Gituser is a website that allows you to enter a username and get all relevant information about a GitHub user or organization, including their followers, following, and repositories.",
-      images: [
-        `${git1}`,
-        `${git1}`,
-        `${git1}`,
-        
-      ],
+      images: [`${git1}`, `${weather1}`, `${weather2}`],
     },
     {
       title: "Weather App",
       url: "https://weatherapp.navin.bio",
       description:
         "Weather app is a website where you will find multiple tools on a single page, which includes week's forecast, Temperature, Weather mode, UV, Wind, Pressure, Humidity, Sunset/sunrise time and much more.",
-      images: [
-        `${git1}`,
-        `${git1}`,
-        `${git1}`,
-      ],
+      images: [`${weather1}`, `${weather2}`, `${git1}`],
     },
     {
       title: "Coffee Website",
       url: "https://coffeweb.navin.bio",
       description:
         "I developed a website for a coffee shop, showcasing their menu, story, and brand identity. . I used React JavaScript , Tailwind ,Html  to build the website, and implemented responsive design to ensure a seamless experience across various devices.",
-      images: [
-        `${git1}`,
-        `${git1}`,
-        `${git1}`,
-      ],
+      images: [`${git1}`, `${git1}`, `${git1}`],
     },
     {
       title: "To-do List",
       url: "https://to-do.navin.bio",
       description:
         "These projects highlight my ability to create dynamic and responsive user interfaces, manage state efficiently and implement create, read, delete operations. This app showcases my skills in HTML, CSS and JavaScript.",
-      images: [
-        `${git1}`,
-        `${git1}`,
-        `${git1}`,
-      ],
+      images: [`${git1}`, `${git1}`, `${git1}`],
     },
     {
       title: "News",
       url: "https://news.navin.bio",
       description:
         "News is a React app where you will find news highlights. This data is provided by the NYT API and you can sort news by category and you can also search for articles.",
-      images: [
-        `${git1}`,
-        `${git1}`,
-        `${git1}`, 
-      ],
+      images: [`${git1}`, `${git1}`, `${git1}`],
     },
   ];
 
@@ -82,25 +63,25 @@ function Projects() {
             className="border-t-[1px] border-b-[1px]  border-[#939396] p-4 rounded-lg project_card bg-[#0f0d0d]   hover:border-t-[1px] transition-all duration-300"
           >
             <div className="flex justify-center">
-  <div className="w-[90%]  p-2">
-    <Swiper
-      effect="cards"
-      grabCursor={true}
-      modules={[EffectCards, Pagination]}
-      className="mySwiper"
-    >
-      {project.images.map((image, imageIndex) => (
-        <SwiperSlide key={imageIndex} className="rounded-lg">
-          <img
-            src={image}
-            alt={`${project.title} - Image ${imageIndex + 1}`}
-            className="w-full  object-fill rounded-lg border-[15px]  border-blue-500"
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  </div>
-</div>
+              <div className="w-[90%]  ">
+                <Swiper
+                  effect="cards"
+                  grabCursor={true}
+                  modules={[EffectCards, Pagination]}
+                  className="mySwiper"
+                >
+                  {project.images.map((image, imageIndex) => (
+                    <SwiperSlide key={imageIndex} className="rounded-lg">
+                      <img
+                        src={image}
+                        alt={`${project.title} - Image ${imageIndex + 1}`}
+                        className="w-full  object-cover rounded-lg border-[15px]  border-pink-300"
+                      />
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              </div>
+            </div>
 
             <Link
               to={project.url}
